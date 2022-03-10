701a85ec06a16d501380b0fa523e0b27c9fcc1f8
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-03-10 10:20:37 +0200

    password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-// modleing for users
+// adding middleware
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     requited: true,
@@ -42,5 +43,17 @@ const User = mongoose.model("User", {
     },
   },
 });
+
+userSchema.pre("save", async function (next) {
+  const user = this; // easy to navigate the code
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
