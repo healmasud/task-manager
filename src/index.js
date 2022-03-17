f9004ba9fe14f5705ff20d96ed46a3ed1d78a4cf
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-03-17 14:13:13 +0200

    quick bug fix

--- src/index.js
+++ src/index.js
@@ -15,14 +15,3 @@ app.listen(port, () => {
 });
 
 const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task. findById('5c2e505a3253e18a43e612e6')
-  // await task. populate(' owner'). execPopulate()
-  // console. log (task. owner)
-  const user = await User.findById("6232fb487e599e3365c23332");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
-
-main();
